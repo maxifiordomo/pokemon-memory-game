3dcc9515ea8dbec3697f80f6313416f888b2364a
Maximiliano Fiordomo <maxifiordomo2001@gmail.com>
2021-07-29 01:27:51 -0300

    pokemon memo-game

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+
+import "./index.css";
+
+
+const url = "https://pokeres.bastionbot.org/images/pokemon";
+
+const pokemons = [
+  { id: 1, name: "balbasaur" },
+  { id: 8, name: "wartotle" },
+  { id: 9, name: "blastoise" },
+  { id: 6, name: "charizard" }
+];
 
 function App() {
+
+  const [flipCard, setFlipCard] = useState([])
+
+  const [matched, setMatched] = useState([])
+
+  const pairOfPokemons = [...pokemons, ...pokemons]
+
+  const handleFlip = (index) => {
+    setFlipCard((opened) => [...opened, index])
+  }
+
+  useEffect(() => {
+    const firstMatch = pairOfPokemons[flipCard[0]];
+    const secondMatch = pairOfPokemons[flipCard[1]];
+
+    if (secondMatch && firstMatch.id === secondMatch.id) {
+      setMatched([...matched, firstMatch.id])
+    }
+    if (flipCard.length === 2) {
+      setTimeout(() => setFlipCard([]), 1000);
+    }
+
+  }, [flipCard])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="cards">
+        {
+          pairOfPokemons.map((pokemon, index) => {
+
+            let isFlipped = false;
+
+            if (flipCard.includes(index)) {
+              isFlipped = true;
+            }
+
+            if (matched.includes(pokemon.id)) {
+              isFlipped = true;
+            }
+
+            return (
+              <div className={`pokemon-card ${isFlipped ? "flipped" : ""} `} key={index}
+                onClick={() => handleFlip(index)}>
+                <div className="inner">
+                  <div className="front">
+                    <img src={`${url}/${pokemon.id}.png`} alt="pokemon" width="100" />
+                  </div>
+                  <div className="back"></div>
+                </div>
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   );
 }
